38ea3039fc907a529c1c1e50996f92fc50d90751
jose-escobedo <escobedo.jose48@outlook.com>
2025-06-26 20:47:53 -0700

    tranquil name changes.

--- app/about/page.js
+++ app/about/page.js
@@ -4,9 +4,9 @@ import AboutTeamSection from '@/components/AboutTeamSection';
 import BreadcrumbBanner from '@/components/BreadcrumbBanner';
 
 export const metadata = {
-  title: 'About Us | Golden Scales Family Therapy',
+  title: 'About Us | Tranquil Path Therapy',
   description:
-    'Learn more about Golden Scales Family Therapy and our mission to support individuals and families through compassionate, effective therapy.',
+    'Learn more about Tranquil Path Therapy and our mission to support individuals and families through compassionate, effective therapy.',
 };
 
 export default function AboutPage() {
--- app/services/page.js
+++ app/services/page.js
@@ -4,9 +4,9 @@ import ServicesDetailTabs from '@/components/ServicesTabs';
 import CallToBook from '@/components/CallToBook';
 
 export const metadata = {
-  title: 'Services | Golden Scales Family Therapy',
+  title: 'Services | Tranquil Path Therapy',
   description:
-    'Explore the full range of therapeutic services offered by Golden Scales Family Therapy to support your mental health and well-being.',
+    'Explore the full range of therapeutic services offered by Tranquil Path Therapy to support your mental health and well-being.',
 };
 
 export default function ServicesPage() {
